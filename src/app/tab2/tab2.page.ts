81ba8cd38eda37d9ed1d58e82b5f12ef78ab5e60
Samet Taş <bmsamettas@gmail.com>
2023-11-02 15:46:02 +0300

    gallery-app

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  constructor(public photoService: PhotoService) { }
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
 
-  constructor() {}
+addPhotoToGallery() {
+  this.photoService.addNewToGallery();
+}
 
 }
+
+
